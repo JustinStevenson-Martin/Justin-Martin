3106ad8d805ca5a1b5a97b6085884f5f9d189faf
Justin249 <justinmartin249@gmail.com>
2021-09-02 11:15:31 +0100

    Added JS for all nav buttons Test

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 const BackTop = document.querySelector("#BackTop");
 
-    BackTop.addEventListener("click", function() {
-      window.scrollTo(0, 0);
-    })
+  BackTop.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+  })
+
+const AboutBtn = document.querySelector("#AboutBtn");
+
+  AboutBtn.addEventListener("click", function() {
+    window.scrollTo(0, 1350);
+  })
+
+const ProjectsBtn = document.querySelector("#ProjectsBtn");
+
+  ProjectsBtn.addEventListener("click", function() {
+    window.scrollTo(0, 3690);
+  })
+
+const TechnologyBtn = document.querySelector("#TechnologyBtn");
+
+  TechnologyBtn.addEventListener("click", function() {
+    window.scrollTo(0, 5900);
+  })
+
+const ContactBtn = document.querySelector("#ContactBtn");
+
+  ContactBtn.addEventListener("click", function() {
+    window.scrollTo(0, 8350);
+  })
