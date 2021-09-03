1b68b75560f4b5589488e3289595313ed7455030
Justin249 <justinmartin249@gmail.com>
2021-09-03 12:20:11 +0100

    Test

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const BackTop = document.querySelector("#BackTop");
 
-  BackTop.addEventListener("click", function() {
-    window.scrollTo(0, 0);
-  })
+BackTop.addEventListener("click", function() {
+  window.scrollTo(0, 0);
+})
 
 const AboutBtn = document.querySelector("#AboutBtn");
 
-  AboutBtn.addEventListener("click", function() {
-    window.scrollTo(0, 1300);
-  })
+AboutBtn.addEventListener("click", function() {
+  window.scrollTo(0, 1300);
+})
 
 const ProjectsBtn = document.querySelector("#ProjectsBtn");
 
-  ProjectsBtn.addEventListener("click", function() {
-    window.scrollTo(0, 3705);
-  })
+ProjectsBtn.addEventListener("click", function() {
+  window.scrollTo(0, 3705);
+})
 
 const TechnologyBtn = document.querySelector("#TechnologyBtn");
 
-  TechnologyBtn.addEventListener("click", function() {
-    window.scrollTo(0, 5930);
-  })
+TechnologyBtn.addEventListener("click", function() {
+  window.scrollTo(0, 5930);
+})
 
 const ContactBtn = document.querySelector("#ContactBtn");
 
-  ContactBtn.addEventListener("click", function() {
-    window.scrollTo(0, 7600);
-  })
+ContactBtn.addEventListener("click", function() {
+  window.scrollTo(0, 7600);
+})
