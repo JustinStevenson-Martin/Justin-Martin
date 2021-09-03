4fee746bcc268462798128375d4d24eb4f7fe876
Justin249 <justinmartin249@gmail.com>
2021-09-03 11:11:40 +0100

    Test Improved tech area and landing page

--- main.js
+++ main.js
@@ -7,23 +7,23 @@ const BackTop = document.querySelector("#BackTop");
 const AboutBtn = document.querySelector("#AboutBtn");
 
   AboutBtn.addEventListener("click", function() {
-    window.scrollTo(0, 1350);
+    window.scrollTo(0, 1300);
   })
 
 const ProjectsBtn = document.querySelector("#ProjectsBtn");
 
   ProjectsBtn.addEventListener("click", function() {
-    window.scrollTo(0, 3690);
+    window.scrollTo(0, 3705);
   })
 
 const TechnologyBtn = document.querySelector("#TechnologyBtn");
 
   TechnologyBtn.addEventListener("click", function() {
-    window.scrollTo(0, 5900);
+    window.scrollTo(0, 5930);
   })
 
 const ContactBtn = document.querySelector("#ContactBtn");
 
   ContactBtn.addEventListener("click", function() {
-    window.scrollTo(0, 8350);
+    window.scrollTo(0, 7600);
   })
